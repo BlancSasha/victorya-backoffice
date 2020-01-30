70b2da30a42ae4642e1ed4377cbf7bb4c35078ad
Francois Blanc <francois.blanc73@gmail.com>
2020-01-30 11:25:18 +0100

    Initial commit after generation with lumberjack

---
+++ routes/survey-patients.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { survey_patients } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('survey_patients');
+
+// This file contains the logic of every route in Forest Admin for the collection survey_patients:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Survey Patient
+router.post('/survey_patients', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Survey Patient
+router.put('/survey_patients/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Survey Patient
+router.delete('/survey_patients/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Survey Patients
+router.get('/survey_patients', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Survey Patients
+router.get('/survey_patients/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Survey Patient
+router.get('/survey_patients/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Survey Patients
+router.get('/survey_patients.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+module.exports = router;
